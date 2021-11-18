9a46f9c454b92077566356b9583ee909c9851471
unknown <reamybjaoui8@glmail.com>
2021-11-18 09:29:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyForm from './components/MyForm';
+
+
+function App() {
+  return (
+    <div className="App">
+    <MyForm/>npmnpm sr
+    </div>
+  );
+}
+
+export default App;
